ccb454ef61255596cebd32b4bbdfe4b293502231
Austin Andrews <austinandrews@berkeley.edu>
2022-04-18 16:40:56 -0700

    setting up hello world

---
+++ client/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+//import App from './containers/App.jsx';
+// import styles from './styles/application.scss';
+document.addEventListener('DOMContentLoaded', () => {
+   ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<h1>Hello, world!</h1>);
+});
+
+
+
+// render(
+//     {/* <App /> */}
+// , document.getElementById('root'));
+
+
+// ReactDOM
+//   .createRoot(document.getElementById('root'))
+//   .render(<h1>Hello, world!</h1>);
